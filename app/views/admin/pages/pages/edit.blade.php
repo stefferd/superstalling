f8ecbe425d45d25a51ee1f755b7e377b0190c7ee
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-15 12:46:59 +0200

    Added the ckEditor to the edit file

--- app/views/admin/pages/pages/edit.blade.php
+++ app/views/admin/pages/pages/edit.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
     <div class="col-xs-12">
-    {{ Form::model($page, array('route' => array('admin.pages.update', $page->id))) }}
+        {{ Form::model($page, array('route' => array('admin.pages.update', $page->id))) }}
         <div class="form-group">
             {{ Form::label('title', Lang::get('pages.title'), array('class' => 'control-label')) }}
             {{ Form::text('title', null, array('placeholder' => 'Pagina titel', 'class' => 'form-control')) }}
@@ -18,7 +18,7 @@
         </div>
         <div class="form-group">
             {{ Form::label('content', Lang::get('pages.content'), array('class' => 'control-label')) }}
-            {{ Form::textarea('content', null, array('placeholder' => 'Inhoud van de pagina', 'class' => 'form-control')) }}
+            {{ Form::textarea('content', null, array('placeholder' => 'Inhoud van de pagina', 'class' => 'form-control', 'id' => 'ckeditor')) }}
             {{ $errors->first('content') }}
         </div>
         <div class="form-group">
@@ -31,6 +31,7 @@
             {{ Form::checkbox('active') }}
         </div>
         <h2>Meta data</h2>
+
         <div class="form-group">
             {{ Form::label('keywords', Lang::get('pages.keywords'), array('class' => 'control-label')) }}
             {{ Form::text('keywords', null, array('placeholder' => 'Trefwoorden...', 'class' => 'form-control')) }}
@@ -45,6 +46,12 @@
             {{ Form::button(Lang::get('admin.action_save'), array('class' => 'btn btn-primary', 'type' => 'submit')) }}
             <a href="{{ URL::route('admin.pages.index') }}">{{ Lang::get('admin.action_cancel') }}</a>
         </div>
-    {{ Form::close() }}
+        {{ Form::close() }}
     </div>
+    <script type="text/javascript" src="{{ URL::asset('assets/js/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor
+        // instance, using default configuration.
+        CKEDITOR.replace('ckeditor');
+    </script>
 @stop
