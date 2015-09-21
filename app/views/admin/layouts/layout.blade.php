ca7b3a1660cf7df54f7fe665bb1f2e3f9548b0fb
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 15:37:00 +0200

    Tweaked the panels

--- app/views/admin/layouts/layout.blade.php
+++ app/views/admin/layouts/layout.blade.php
@@ -4,18 +4,16 @@
     @include('admin.includes.head')
 </head>
 <body class="@yield('title')">
-    <div class="container-fluid">
-        <div class="left-panel">
-            @include('admin.includes.navigation')
-        </div>
-        <main class="main-panel">
-            <div class="container-fluid">
-                <h1>@yield('title')</h1>
-                @yield('content')
-                @include('admin.includes.footer')
-            </div>
-        </main>
-        <div class="right-panel"></div>
+    <div class="left-panel">
+        @include('admin.includes.navigation')
     </div>
+    <main class="main-panel">
+        <div class="container-fluid">
+            <h1>@yield('title')</h1>
+            @yield('content')
+            @include('admin.includes.footer')
+        </div>
+    </main>
+    <div class="right-panel"></div>
 </body>
 </html>
