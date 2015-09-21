8a41e300f01bf68b6cd320136d37d065e9213b9f
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 11:19:19 +0200

    Changed the mail config

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => '',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'postmaster@mg.classiccarseurope.eu', 'name' => 'Classiccarseurope.eu'),
+	'from' => array('address' => 'info@superstalling.nl', 'name' => 'Superstalling'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'postmaster@mg.classiccarseurope.eu',
+	'username' => '',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'emiel',
+	'password' => '',
 
 	/*
 	|--------------------------------------------------------------------------
