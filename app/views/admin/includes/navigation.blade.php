6af778243487a31289453b8d99204feaa0a7c595
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 12:57:06 +0200

    Tweaked the backend navigation

--- app/views/admin/includes/navigation.blade.php
+++ app/views/admin/includes/navigation.blade.php
@@ -4,10 +4,10 @@
             <li><a href="{{ URL::route('admin.dashboard.index') }}">{{ Lang::get('admin.dashboard') }}</a></li>
             <li><a href="{{ URL::route('admin.users.index') }}">{{ Lang::get('users.menu') }}</a></li>
             <li><a href="{{ URL::route('admin.pages.index') }}">{{ Lang::get('pages.menu') }}</a></li>
-            <li><a href="{{ URL::route('admin.news.index') }}">{{ Lang::get('news.menu') }}</a></li>
-            <li><a href="{{ URL::route('admin.catalog.index') }}">{{ Lang::get('catalog.menu') }}</a></li>
-            <li><a href="{{ URL::route('admin.newsletter.index') }}">{{ Lang::get('newsletter.menu') }}</a></li>
-            <li><a href="{{ URL::route('admin.subscriber.index') }}">{{ Lang::get('subscriber.menu') }}</a></li>
+            {{--<li><a href="{{ URL::route('admin.news.index') }}">{{ Lang::get('news.menu') }}</a></li>--}}
+            {{--<li><a href="{{ URL::route('admin.catalog.index') }}">{{ Lang::get('catalog.menu') }}</a></li>--}}
+            {{--<li><a href="{{ URL::route('admin.newsletter.index') }}">{{ Lang::get('newsletter.menu') }}</a></li>--}}
+            {{--<li><a href="{{ URL::route('admin.subscriber.index') }}">{{ Lang::get('subscriber.menu') }}</a></li>--}}
             <li><a href="{{ URL::route('admin.settings.index') }}">{{ Lang::get('settings.menu') }}</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
