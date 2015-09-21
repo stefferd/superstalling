c550507b1905fda152ceece104f5a2461ff826c5
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 17:06:05 +0200

    Visualise the active menu item with javascript

--- app/views/admin/includes/navigation.blade.php
+++ app/views/admin/includes/navigation.blade.php
@@ -1,20 +1,20 @@
 @if (Auth::check())
     <div class="navigation">
         <ul class="navigation-top" role="navigation">
-            <li @if ($title == Lang::get('admin.dashboard'))class="active"@endif>
+            <li>
                 <a href="{{ URL::route('admin.dashboard.index') }}">{{ Lang::get('admin.dashboard') }}</a>
             </li>
-            <li @if ($title == Lang::get('users.menu'))class="active"@endif>
+            <li>
                 <a href="{{ URL::route('admin.users.index') }}">{{ Lang::get('users.menu') }}</a>
             </li>
-            <li @if ($title == Lang::get('pages.menu'))class="active"@endif>
+            <li>
                 <a href="{{ URL::route('admin.pages.index') }}">{{ Lang::get('pages.menu') }}</a>
             </li>
             {{--<li><a href="{{ URL::route('admin.news.index') }}">{{ Lang::get('news.menu') }}</a></li>--}}
             {{--<li><a href="{{ URL::route('admin.catalog.index') }}">{{ Lang::get('catalog.menu') }}</a></li>--}}
             {{--<li><a href="{{ URL::route('admin.newsletter.index') }}">{{ Lang::get('newsletter.menu') }}</a></li>--}}
             {{--<li><a href="{{ URL::route('admin.subscriber.index') }}">{{ Lang::get('subscriber.menu') }}</a></li>--}}
-            <li @if ($title == Lang::get('settings.menu'))class="active"@endif>
+            <li>
                 <a href="{{ URL::route('admin.settings.index') }}">{{ Lang::get('settings.menu') }}</a>
             </li>
         </ul>
@@ -23,4 +23,10 @@
             <li><a href="{{ URL::route('admin.logout') }}">{{ Lang::get('admin.action_logout') }}</a></li>
         </ul>
     </div>
+    <script>
+       $('.navigation-top').find('li').on('click', function() {
+           $('.navigation-top li').removeClass('active');
+           $(this).addClass('active');
+       })
+    </script>
 @endif
