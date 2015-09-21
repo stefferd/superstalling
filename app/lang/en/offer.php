6d9a8c026d425f69508a517769c22204f494e4b6
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 17:24:32 +0200

    Added the translations

---
+++ app/lang/en/offer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefvandenberg
+ * Date: 06/11/14
+ * Time: 23:21
+ */
+
+return array(
+    'menu' => 'Offerte',
+    'email' => 'Emailadres waar de offerte vandaag gestuurd moet worden',
+    'key' => 'Sleutel',
+    'value' => 'Waarde',
+    'created_at' => 'Aangemaakt op',
+    'updated_at' => 'Aangepast op',
+    'headtitle' => 'Offerte instellingen beheren',
+    'index' => 'Offerte instellingen',
+    'edit' => 'Offerte instelling bewerken'
+);
