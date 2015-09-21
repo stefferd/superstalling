42991dedb7b11c156556102d5d7aac3bee925190
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 17:03:24 +0200

    Visualise the active menu item

--- app/views/admin/includes/navigation.blade.php
+++ app/views/admin/includes/navigation.blade.php
@@ -1,14 +1,22 @@
 @if (Auth::check())
     <div class="navigation">
         <ul class="navigation-top" role="navigation">
-            <li><a href="{{ URL::route('admin.dashboard.index') }}">{{ Lang::get('admin.dashboard') }}</a></li>
-            <li><a href="{{ URL::route('admin.users.index') }}">{{ Lang::get('users.menu') }}</a></li>
-            <li><a href="{{ URL::route('admin.pages.index') }}">{{ Lang::get('pages.menu') }}</a></li>
+            <li @if ($title == Lang::get('admin.dashboard'))class="active"@endif>
+                <a href="{{ URL::route('admin.dashboard.index') }}">{{ Lang::get('admin.dashboard') }}</a>
+            </li>
+            <li @if ($title == Lang::get('users.menu'))class="active"@endif>
+                <a href="{{ URL::route('admin.users.index') }}">{{ Lang::get('users.menu') }}</a>
+            </li>
+            <li @if ($title == Lang::get('pages.menu'))class="active"@endif>
+                <a href="{{ URL::route('admin.pages.index') }}">{{ Lang::get('pages.menu') }}</a>
+            </li>
             {{--<li><a href="{{ URL::route('admin.news.index') }}">{{ Lang::get('news.menu') }}</a></li>--}}
             {{--<li><a href="{{ URL::route('admin.catalog.index') }}">{{ Lang::get('catalog.menu') }}</a></li>--}}
             {{--<li><a href="{{ URL::route('admin.newsletter.index') }}">{{ Lang::get('newsletter.menu') }}</a></li>--}}
             {{--<li><a href="{{ URL::route('admin.subscriber.index') }}">{{ Lang::get('subscriber.menu') }}</a></li>--}}
-            <li><a href="{{ URL::route('admin.settings.index') }}">{{ Lang::get('settings.menu') }}</a></li>
+            <li @if ($title == Lang::get('settings.menu'))class="active"@endif>
+                <a href="{{ URL::route('admin.settings.index') }}">{{ Lang::get('settings.menu') }}</a>
+            </li>
         </ul>
         <ul class="navigation-bottom" role="navigation">
             <li><a>{{ Auth::user()->name }}</a></li>
