afe9030ef543c99de58b61ede177f69114bc8239
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 14:51:26 +0200

    Aligned the footer with the rest

--- app/views/admin/includes/footer.blade.php
+++ app/views/admin/includes/footer.blade.php
@@ -1,1 +1,7 @@
-<footer class="copyright text-right">Copyright &copy Debmedia</footer>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-md-4 col-md-offset-4">
+            <footer class="copyright text-right">Copyright &copy DEBmedia</footer>
+        </div>
+    </div>
+</div>
