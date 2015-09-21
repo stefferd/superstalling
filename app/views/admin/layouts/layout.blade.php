b52f1f3b97ccca3529bd0bc67b31c2927f2aaeb3
Stef van den Berg <stef.vandeberg@webpower.nl>
2015-09-21 15:45:04 +0200

    Changed the classnames to the css names

--- app/views/admin/layouts/layout.blade.php
+++ app/views/admin/layouts/layout.blade.php
@@ -4,16 +4,16 @@
     @include('admin.includes.head')
 </head>
 <body class="@yield('title')">
-    <div class="left-panel">
+    <div class="panel-left">
         @include('admin.includes.navigation')
     </div>
-    <main class="main-panel">
+    <main class="panel-main">
         <div class="container-fluid">
             <h1>@yield('title')</h1>
             @yield('content')
             @include('admin.includes.footer')
         </div>
     </main>
-    <div class="right-panel"></div>
+    <div class="panel-right"></div>
 </body>
 </html>
